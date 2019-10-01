5cf9aea2dc55d8b92b8ee489fca0e34193aeeeb2
Thiago-F <thiago_fialho150@hotmail.com>
2019-10-01 19:53:47 -0300

    init backend

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import routes from './routes'
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.get('/' , (req , res) => {
+    return res.json({ok : true})
+})
+
+app.listen(3333);
