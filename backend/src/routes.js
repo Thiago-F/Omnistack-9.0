c9023d4806d96eb242f566d37207ff87ff655080
Thiago-F <thiago_fialho150@hotmail.com>
2019-10-02 09:39:06 -0300

    criacao de sessao de usuario com email

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,9 @@
 import {Router} from 'express'
 
+import SessionController from './controllers/SessionController'
+
 const routes = Router();
+
+routes.post('/sessions', SessionController.store)
 
 export default routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import routes from './routes'
+import mongoose from 'mongoose'
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-wtgxs.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 app.use(express.json());
 app.use(routes);
 
-app.get('/' , (req , res) => {
-    return res.json({ok : true})
-})
 
 app.listen(3333);
